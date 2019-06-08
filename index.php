bab87bf612e63a762e70646bb2ab0c96ddd2e2b6
Unknown <rzviqbal7@gmail.com>
2019-06-08 20:57:39 +0400

    Website is Live

--- index.php
+++ index.php
@@ -3,22 +3,32 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Website coming soon</title>
+    <title>Rizvi Iqbal</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- CSS FILES -->
-    <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link crossorigin="anonymous" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" rel="stylesheet">
     <!-- JS FILES -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <!-- <script src="main.js"></script> -->
 </head>
-<body>
-    <div class="container-fluid">
-        <img src="images/soon.png" class="soon-img">
+<body >
+    <div class="container-fluid d-flex justify-content-center align-items-center main-content">
+        <div class="container">
+             Hello, I'm <br>
+             <span class="name">Rizvi Iqbal</span><br>
+            Full-Stack Web Developer
+            <div class="buttons mt-2">
+                <a href="https://www.linkedin.com/in/rizviqbal/" class="btn btn-outline-primary btn-sm"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://github.com/Rizviqbal711" class="btn btn-outline-secondary btn-sm"><i class="fab fa-github"></i></a>
+                <a href="pdf/resume.pdf" class="btn btn-outline-info btn-sm">Resume</a>
+            </div> 
+        </div>
     </div>
 </body>
 </html>
