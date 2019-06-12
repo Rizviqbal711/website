a0ded1fea8b3b809b3be47e41063a4226c19d449
Unknown <rzviqbal7@gmail.com>
2019-06-12 15:49:16 +0400

    Removed Google Analystics and added Hotjar

--- index.php
+++ index.php
@@ -6,14 +6,16 @@
     <title>Rizvi Iqbal</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141912931-1"></script>
+    <!-- Hotjar Tracking Code for rzv.io -->
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-141912931-1');
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:1360086,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
     </script>
     <!-- CSS FILES -->
     <link rel="stylesheet" type="text/css" href="css/main.css" />
